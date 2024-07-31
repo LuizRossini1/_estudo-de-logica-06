f5b4a356fbf89841f9e7480f9026e921ce431d60
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-31 19:55:01 -0300

    implementing logic

--- src/Challenges/exercise01/Main.java
+++ src/Challenges/exercise01/Main.java
@@ -1,5 +1,13 @@
+/* Crie um programa em Java que utilize as classes HttpClient, HttpRequest
+e HttpResponse para fazer uma consulta à API do Google Books.
+Solicite ao usuário que insira o título de um livro,
+e exiba as informações  disponíveis sobre o livro retornado pela API. */
 package Challenges.exercise01;
 
 public class Main {
+    public static void main(String[] args) {
+        Service service = new Service();
 
+        service.getTitle("ola");
+    }
 }
---
+++ src/Challenges/exercise01/Service.java
@@ -0,0 +1,33 @@
+package Challenges.exercise01;
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class Service {
+    Scanner scanner = new Scanner(System.in);
+    private String title = scanner.nextLine();
+    private String apiKey = "AIzaSyB7YH_lp2esNcxLa-JxyK5h6SiioXcNJrs";
+
+    public String getTitle(String title) {
+        return title;
+    }
+
+    HttpClient client = HttpClient.newHttpClient();
+    HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("https://www.googleapis.com/books/v1/volumes?q=" + title + "&key=" + apiKey)).build();
+    HttpResponse<String> response;
+
+    {
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
