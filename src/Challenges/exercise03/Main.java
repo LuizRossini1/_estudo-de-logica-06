d45e71f5a56a53d71e30417d5aa9469b8b6d0527
LuizRossini <luiz.guirossini123@gmail.com>
2024-08-02 20:39:33 -0300

    creating exercise 3 body

--- src/Challenges/exercise03/Main.java
+++ src/Challenges/exercise03/Main.java
@@ -1,5 +1,12 @@
+/* 3 - Crie um programa Java que faça uma consulta à API do TheMealDB utilizando as classes
+HttpClient, HttpRequest e HttpResponse. Solicite ao usuário que insira o nome de uma receita e exiba as
+informações disponíveis sobre essa receita. */
 package Challenges.exercise03;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter a recipe");
 
+        var test = new Service();
+    }
 }
