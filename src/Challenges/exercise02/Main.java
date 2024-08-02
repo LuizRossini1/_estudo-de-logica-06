9ec10b18603767f897d6539a3fed2c6ed347e6e2
LuizRossini <luiz.guirossini123@gmail.com>
2024-08-02 20:08:26 -0300

    structuring class 'service'

--- src/Challenges/exercise02/Main.java
+++ src/Challenges/exercise02/Main.java
@@ -3,5 +3,9 @@ para fazer uma consulta à API CoinGecko e exiba a cotação atual de uma cripto
 package Challenges.exercise02;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter the cryptocurrency name to see the quote");
 
+        var service = new Service();
+    }
 }
--- src/Challenges/exercise02/Service.java
+++ src/Challenges/exercise02/Service.java
@@ -5,14 +5,26 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class Service {
+    Scanner scanner = new Scanner(System.in);
+    private String criptoName = scanner.nextLine();
+
     HttpClient client = HttpClient.newHttpClient();
     HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create(""))
+            .uri(URI.create("https://api.coingecko.com/api/v3/simple/price?ids=" +criptoName+ "&vs_currencies=brl"))
             .build();
-    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+    HttpResponse<String> response;
 
-    public Service() throws IOException, InterruptedException {
+    {
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
