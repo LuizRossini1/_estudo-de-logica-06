1912fd6b069d61b3614176038874173c5083f707
LuizRossini <luiz.guirossini123@gmail.com>
2024-08-06 17:52:13 -0300

    refactoring massage on main class

--- src/Challenges/exercise01/Main.java
+++ src/Challenges/exercise01/Main.java
@@ -6,7 +6,7 @@ package Challenges.exercise01;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Enter the name of the book you want to search for");
+        System.out.println("Enter the name of the book you want to search for: ");
 
         Service service = new Service();
         service.getTitle();
